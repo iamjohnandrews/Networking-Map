3626284218f9003fcb543f012dd16f3bc7079f81
iamjohnandrews <johnnydrews@gmail.com>
2016-01-16 13:42:23 -0500

    add ui to story board and setting u networking

--- Networking-Map/ViewController.m
+++ Networking-Map/ViewController.m
@@ -8,6 +8,7 @@
 
 #import "ViewController.h"
 
+NSString *const citrixProjectURL = @"http://nsmith.nfshost.com/sf/contacts.json";
 @interface ViewController ()
 
 @end
@@ -16,12 +17,49 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self retrieveContactsJSON];
+    
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark Networking Methods
+
+- (void)retrieveContactsJSON {
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:citrixProjectURL]];
+    
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]];
+    
+    [[session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        if (!error && data) {
+            NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:data
+                                                                 options:0
+                                                                   error:nil];
+            NSLog(@"retrieved from the Internet %@", dict);
+            [self parseResponse:dict];
+        }
+    }] resume];
 }
+
+- (void)parseResponse:(NSDictionary *)jsonDict {
+    
+}
+
+
+
+#pragma mark TableView DataSource Methods
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 1;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    UITableViewCell *cell = [[UITableViewCell alloc] init];
+    return cell;
+}
+
+#pragma mark TableView Delegate Methods
+
 
 @end
