0725d77e7289a83afe201ad7a99e32e9184ffbce
iamjohnandrews <johnnydrews@gmail.com>
2016-01-16 12:59:56 -0500

    first commit

---
+++ Networking-Map/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Networking-Map
+//
+//  Created by John Andrews on 1/16/16.
+//  Copyright © 2016 John Andrews. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Networking-Map/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Networking-Map
+//
+//  Created by John Andrews on 1/16/16.
+//  Copyright © 2016 John Andrews. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
